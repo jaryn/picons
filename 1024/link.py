445ab3d2228dc23aa2bfa5d4137eb0a139d0db74
Jaroslav Henner <jhenner@redhat.com>
2020-02-09 18:21:16 +0100

    Make higher success rate

--- 1024/link.py
+++ 1024/link.py
@@ -6,17 +6,44 @@ with open('channels') as channels:
     matches = [re.search(M, line) for line in channels]
 
 matches = [m.group(1) for m in matches if m]
-mapping = {k: k.replace(" ", "").replace("|","").lower() for k in matches if k }
+
+def commonreplace(s):
+    return s.replace(".", "").replace("+", "plus").replace("+", "plus")
+
+def makesrc(s):
+    """ This is the file with the content """
+    s = commonreplace(s)
+    s = s.replace('CRo D-DUR', 'croddur')
+    s = s.replace("| T2","")
+    s = s.replace("| P", "")
+    s = s.replace("| T", "")
+    s = s.replace(":D/", "D")
+    s = s.replace(" ", "")
+    s = s.lower()
+    return s
+
+def maketgt(s):
+    """ This is what will be in the url (from tvheadend)"""
+    s = commonreplace(s).strip()
+    s = s.replace("|", "_")
+    s = s.replace(":D/", "_D-")
+    return s
 
 add = {
     'CT 1 SM HD T2': 'ct1hdt2',
 }
 
-mapping.update(add)
-
-for tgt, src in mapping.items():
+linked = failed = 0
+for channel in sorted(set(matches)):
+    src = makesrc(channel)
+    tgt = maketgt(channel)
+    print(f"For channel {channel:<20}\tcrating {src} -> {tgt}", end="")
     if os.path.isfile(f"{src}.png"):
-        print(f"linking: {src}")
         os.link(f"{src}.png", f"dvb-{tgt}.png")
+        linked += 1
     else:
-        print(f"{src}.png doesn't exist")
+        print(f"\tthe source file doesn't exist", end="")
+        failed += 1
+    print()
+
+print(f"Linked: {linked}, failed: {failed}")
